f99fbefc4572c8641014a71d5bee0c27e06cc638
sunitmal <81052111+sunitmal@users.noreply.github.com>
2021-05-25 06:55:10 +0100

    Add files via upload

---
+++ SunitMalWK10HLT.R
@@ -0,0 +1,32 @@
+# TASK 1 #
+
+name<-readline("Enter your name")
+age<-readline("Enter your age")
+hello <-paste("Welcome",name,"you are",age,"years old")
+print(hello)
+
+# TASK 2 #
+
+name = "Python"; 
+n1 =  10; 
+n2 =  0.5
+nums = c(10, 20, 30, 40, 50, 60)
+print(ls())
+print("Details of the objects in memory:")
+print(ls.str())
+
+# TASK 3 #
+
+print("Sequence of numbers from 20 to 50:")
+print(seq(20,50))
+print("Mean of numbers from 20 to 60:")
+print(mean(20:60))
+print("Sum of numbers from 51 to 91:")
+print(sum(51:91))
+
+# TASK 4 # 
+
+v = sample(-50:50, 10, replace=TRUE)
+print("Content of the vector:")
+print("10 random integer values between -50 and +50:")
+print(v)
